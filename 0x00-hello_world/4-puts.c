ad0aac7856e1022fa0d6eca943295667eb9642be
TCorneille <corneilletwagirimana@gmail.com>
2023-06-15 14:43:37 -0700

    pro4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - C programming 
- * Return:0(success)
+i * Return:0(success)
  */
+
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
