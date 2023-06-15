1388385d822d01ac5dc677e2c8b94cc554141e7f
TCorneille <corneilletwagirimana@gmail.com>
2023-06-15 14:15:59 -0700

    pro4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,6 @@
  * Return:0(success)
  */
 int main(void)
-{
-puts("\"programming is like building a multilingual puzzle");
+{puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
-
