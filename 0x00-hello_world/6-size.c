d4f4f66aca984a10ed33461f321ed648db6d825f
TCorneille <corneilletwagirimana@gmail.com>
2023-06-15 20:53:08 -0700

    pro6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Size
+ * Return ,0 (Success)
+ */
+{
+	int a;
+	char b;
+	long int c;
+	long int d;
+	float f;
+printf("size of a char:%lu byte(s)\n",(unsigned long)sizeof (b));
+printf("size of an int:%lu byte(s)\n),(unsigned long)sizeof (a));
+printf("size of a long int:%lu byte(s)\n",(unsigned long)sizeof (c));
+prinf("size of a long int:%lu byte(s)\n",(unsigned long)sizeof (d));
+printf("size of a float:%lu byte(s)\n",(unsigned long)sizeof (f));
+return (0);
+}
+
