94ad71674d3881bdac7d70afc37330c37ab12306
TCorneille <corneilletwagirimana@gmail.com>
2023-06-19 14:25:09 -0700

    pro3
    pro3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+*main - Data Entry
+* Return 0 succes
+*/
+int main(void)
+{
+	 char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+		putchar le;
+	for  (le = 'A'; le <= 'Z'; le++)
+		putchar le;
+	putchar("\n")
+
+	return (0);
+}
