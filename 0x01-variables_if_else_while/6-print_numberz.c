fc646fd4e6e71920a2d8cc28c9ea986e3df69af0
TCorneille <corneilletwagirimana@gmail.com>
2023-06-25 02:39:14 -0700

    pro6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Numbers
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar (i);
+	putchar ('\n');
+	return (0);
+}
