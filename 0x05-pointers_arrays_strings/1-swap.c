84c8c3d25df372eda496948f06b73ed8196af314
TCorneille <corneilletwagirimana@gmail.com>
2023-06-26 16:23:15 -0700

    pro1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swaps two integers
+ * @a: integer1
+ * @b: integer2
+ * Return: nothing
+ */
+void swap_int(int *a, int *b);
+{
+	int t = *a;
+	*a = *b;
+	*b = *t
+}
