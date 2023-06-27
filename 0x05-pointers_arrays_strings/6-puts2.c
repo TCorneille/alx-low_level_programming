13cea8f158d0e3888d22a8fa29e976c8c779cf67
TCorneille <corneilletwagirimana@gmail.com>
2023-06-27 15:47:03 -0700

    pro6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - string
+ * @str: string to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int n = 0;
+	int m;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	for (m = 0; m < n; m += 2)
+	{
+		putchar(*str[m]);
+	}
+	putchar('\n');
+}
