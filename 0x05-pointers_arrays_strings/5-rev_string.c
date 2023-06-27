3f98c87605cc3429a7008e559164af26ce2c5755
TCorneille <corneilletwagirimana@gmail.com>
2023-06-26 22:14:22 -0700

    pro5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - modify a string into the reverse
+ * @s: string to be modified
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int a = 0, n = 0;
+	char t;
+
+	while (s[n++])
+		a++;
+	for (n = a - 1; n >= n / 2; n--)
+	{
+		t = s[n];
+		s[n] = s[a - n - 1];
+		s[a - n - 1] = t;
+	}
+}
+
