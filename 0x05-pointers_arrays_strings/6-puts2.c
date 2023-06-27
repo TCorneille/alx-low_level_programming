1c33be60e325d736d22e7a4efd95085c7d4f3894
TCorneille <corneilletwagirimana@gmail.com>
2023-06-27 14:54:43 -0700

    pro6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * puts_half - half of string
+ * @str: string to print
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+	int m;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	if (n % 2 == 0)
+	{
+		m = (n - 1) / 2;
+		m += 1;
+	}
+	else
+	{
+		m = n / 2
+	}
+	for (; m < n; m++)
+	{
+		putchar(*str[m]);
+	}
+	putchar('\n');
+}
