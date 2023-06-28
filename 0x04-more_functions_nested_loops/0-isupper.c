88263216f5be3c877fb41aaaf131b07fe249a3d9
TCorneille <corneilletwagirimana@gmail.com>
2023-06-28 00:40:39 -0700

    pro0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _is upper - check isupper
+ * @c: number
+ * Return: 1 upper letter 0 not upper
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 98)
+	{
+		return (1);
+	}
+	return (0);
+
+}
