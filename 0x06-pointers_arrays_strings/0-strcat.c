96466b7b9cd3ea2bfb43def27ad3330dbc5530fb
TCorneille <corneilletwagirimana@gmail.com>
2023-07-02 11:21:28 -0700

    pro0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - overwrites string
+ * @dest: string to be appended
+ * @src: to be appended on dest
+ * @n: number from src to be appended to dest
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0; dest_b = 0;
+
+	while (dest[i++])
+		dest_b++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[dest_b++] = src[i];
+	return (dest);
+}
