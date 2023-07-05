2186fcc80f44309304bfac058b6f0fbc598fc986
TCorneille <corneilletwagirimana@gmail.com>
2023-07-05 15:14:56 -0700

    pro 0

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unstdio.h>
+
+/**
+ * _putchar - writes the character c
+ * @c: The character to print
+ * Return: 0 succces 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
