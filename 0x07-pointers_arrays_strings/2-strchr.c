1014dbfa749d0a17b20fe2c0b8247d1db0b0e726
TCorneille <corneilletwagirimana@gmail.com>
2023-07-05 15:48:50 -0700

    pro2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - Entry
+ * @s: destination
+ * @c: constant bytes
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (s + a);
+	}
+	return (NULL);
+}
