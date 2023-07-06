74f604dbad38b6d420ea17e3591dd129fc974f7b
TCorneille <corneilletwagirimana@gmail.com>
2023-07-06 15:18:24 -0700

    pro6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry
+ * @a: array
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, t;
+
+	for (i = 0; i < 8 ; i++)
+	{
+		for (t = 0; t < 8; t++)
+			_putchar(a[i][t]);
+		_putchar('\n');
+	}
+}
