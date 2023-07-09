3bdd2f10cdbf12d47dd24a17d3bddf5ba6c52b2a
TCorneille <corneilletwagirimana@gmail.com>
2023-07-09 00:10:43 -0700

    pro 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial -  factorial of a number
+ * @n: number
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
