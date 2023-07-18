bbe70b4ebabb46c92737dbaaac988934b32d7685
TCorneille <corneilletwagirimana@gmail.com>
2023-07-18 07:54:58 -0700

    pro0;

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - about dog
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
