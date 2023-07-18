47849a89ed74a484b161bf7588c833bb4b84be11
TCorneille <corneilletwagirimana@gmail.com>
2023-07-18 09:06:13 -0700

    pro2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = "(nap)";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = "(nap)";
+		d->owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
