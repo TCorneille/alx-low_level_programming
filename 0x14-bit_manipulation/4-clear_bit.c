3cf0de2e841e91b0b453406d278f7918e1c371b3
TCorneille <corneilletwagiriman@gmail.com>
2023-08-03 07:11:58 -0700

    pro4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include<stdio.h>
+
+/**
+ * clear_bit - sets bit to 0
+ * @n: pointer
+ * @index: index
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
